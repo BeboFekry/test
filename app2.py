0c97525fb1beb1d0422c1c86436f68f2d0c883f9
Abdallah Fekry <116501243+BeboFekry@users.noreply.github.com>
2024-09-27 21:51:41 +0300

    Update app2.py

--- app2.py
+++ app2.py
@@ -18,7 +18,8 @@ wav_audio_data = st_audiorec()
 
 if wav_audio_data is not None:
     if os.path.exists('myfile.wav'):
-        os.remove('myfile.wav')    with open('myfile.wav', mode='xb') as f:
+        os.remove('myfile.wav')
+    with open('myfile.wav', mode='xb') as f:
         f.write(wav_audio_data)
     text = voice_in.speech_to_text(path='myfile.wav')
     st.audio('myfile.wav')
