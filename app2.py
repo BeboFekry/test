cf684c51882dcbf10cef10c5e196cb645f3faea7
Abdallah Fekry <116501243+BeboFekry@users.noreply.github.com>
2024-09-27 21:54:46 +0300

    Update app2.py

--- app2.py
+++ app2.py
@@ -16,15 +16,14 @@ voice_in = VoiceIn()
 
 wav_audio_data = st_audiorec()
 
-# if wav_audio_data is not None:
-if os.path.exists('myfile.wav'):
-    os.remove('myfile.wav')
-if wav_audio_datais not None:
+if wav_audio_data is not None:
+    if os.path.exists('myfile.wav'):
+        os.remove('myfile.wav')
     with open('myfile.wav', mode='xb') as f:
         f.write(wav_audio_data)
-text = voice_in.speech_to_text(path='myfile.wav')
-st.audio('myfile.wav')
-st.session_state.memory.append({'voice':wav_audio_data, 'text':text})
+    text = voice_in.speech_to_text(path='myfile.wav')
+    st.audio('myfile.wav')
+    st.session_state.memory.append({'voice':wav_audio_data, 'text':text})
 # st.rerun()
 
 if st.session_state.memory != []:
